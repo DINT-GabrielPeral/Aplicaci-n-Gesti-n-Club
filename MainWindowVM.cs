08491773fd7920a7c014fec4cfc0512d5e696a66
Gabriel <gabriel.peral@iesdoctorbalmis.com>
2023-03-01 21:06:58 +0100

    Agregar archivos de proyecto.

---
+++ MainWindowVM.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Aplicación_Gestión_Club
+{
+    class MainWindowVM : ObservableObject
+    {
+        private UserControl contenidoMostrar;
+
+        public UserControl ContenidoMostrar
+        {
+            get { return contenidoMostrar; }
+            set { SetProperty(ref contenidoMostrar, value); }
+        }
+
+        public MainWindowVM()
+        {
+
+        }
+
+    }
+}
---
+++ Vistas/Entrar/IniciarSesion/IniciarSesión.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aplicación_Gestión_Club.Vistas.Entrar.IniciarSesion
+{
+    /// <summary>
+    /// Lógica de interacción para IniciarSesión.xaml
+    /// </summary>
+    public partial class IniciarSesión : Window
+    {
+        public IniciarSesión()
+        {
+            InitializeComponent();
+        }
+    }
+}
